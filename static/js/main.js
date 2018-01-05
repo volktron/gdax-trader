f4e11b7a49bbc57a91d77b1f783731436fdb7192
Henrik Volckmer <henrik.volckmer@vonage.com>
2018-01-05 14:35:11 -0500

    Cleanup

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,40 @@
 import axios from 'axios'
 
+class gdax {
+    constructor() {
+        this.BTCUSD = { asks: [[0]] }
+        this.BCHUSD = { asks: [[0]] }
+        this.ETHUSD = { asks: [[0]] }
+    }
+
+    getBook(left, right) {
+        var bookname = left + '-' + right;
+        axios.get('https://api.gdax.com/products/' + bookname + '/book')
+            .then(response => {
+                this[left + right] = response.data
+                document.querySelector('#' + bookname + '-price .bid').innerHTML = this[left + right].bids[0][0];
+                document.querySelector('#' + bookname + '-price .ask').innerHTML = this[left + right].asks[0][0];
+            })
+    }
+
+    update() {
+        var book = ['BTC', 'USD']
+        this.getBook('BTC', 'USD')
+        this.getBook('BCH', 'USD')
+        this.getBook('ETH', 'USD')
+
+        document.querySelector('#BCH-USD-price .btc').innerHTML = (this.BCHUSD.asks[0][0] / this.BTCUSD.asks[0][0]).toFixed(8);
+        document.querySelector('#ETH-USD-price .btc').innerHTML = (this.ETHUSD.asks[0][0] / this.BTCUSD.asks[0][0]).toFixed(8);
+        document.querySelector('title').innerHTML = '$' + this.BCHUSD.asks[0][0];
+    }
+}
+
+gdax = new gdax()
+
 function update() {
     setTimeout(function () {
-        axios.get('https://api.gdax.com/products/BTC-USD/book')
-            .then(function (response) {
-                document.querySelector('#btc-price .bid').innerHTML = response.data.bids[0][0];
-                document.querySelector('#btc-price .ask').innerHTML = response.data.asks[0][0];
-            })
-        axios.get('https://api.gdax.com/products/BCH-USD/book')
-            .then(function (response) {
-                document.querySelector('#bch-price .bid').innerHTML = response.data.bids[0][0];
-                document.querySelector('#bch-price .ask').innerHTML = response.data.asks[0][0];
-                document.querySelector('#bch-price .btc').innerHTML = (response.data.asks[0][0] / document.querySelector('#btc-price .ask').innerHTML).toFixed(4);
-                document.querySelector('title').innerHTML = '$' + response.data.asks[0][0];
-            })
-        axios.get('https://api.gdax.com/products/ETH-USD/book')
-            .then(function (response) {
-                document.querySelector('#eth-price .bid').innerHTML = response.data.bids[0][0];
-                document.querySelector('#eth-price .ask').innerHTML = response.data.asks[0][0];
-                document.querySelector('#eth-price .btc').innerHTML = (response.data.asks[0][0] / document.querySelector('#btc-price .ask').innerHTML).toFixed(4);
-            })
+        gdax.update()
         update()
     }, 2000)
 }
-
 update()
